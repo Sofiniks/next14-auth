b84b28b4d12f1009b5546881910aaa3a815da447
Sofiniks <sofija.nikitina@inbox.lv>
2024-04-14 15:41:01 +0300

    initial commit

---
+++ components/auth/new-verification-form.tsx
@@ -0,0 +1,50 @@
+"use client";
+import { useCallback, useEffect, useState } from "react";
+import { CardWrapper } from "./card-wrapper";
+import { FormError } from "../form-error";
+import { FormSuccess } from "../form-success";
+import { BeatLoader } from "react-spinners";
+import { useSearchParams } from "next/navigation";
+import { newVerification } from "@/actions/new-verification";
+
+export const NewVerificationForm = () => {
+	const [error, setError] = useState<string | undefined>();
+	const [success, setSuccess] = useState<string | undefined>();
+	const searchParams = useSearchParams();
+	const token = searchParams.get("token");
+
+	const onSubmit = useCallback(() => {
+		if (success || error) return;
+		if (!token) {
+			setError("Missing token");
+			return;
+		}
+		newVerification(token)
+			.then((data) => {
+				console.log("Token", token);
+				setSuccess(data.success);
+				setError(data.error);
+			})
+			.catch(() => {
+				setError("Something went wrong!");
+			});
+	}, [token, success, error]);
+
+	useEffect(() => {
+		onSubmit();
+	}, [onSubmit]);
+
+	return (
+		<CardWrapper
+			headerLabel='Confirming your verification'
+			backButtonHref='/auth/login'
+			backButtonLabel='Back to login'
+		>
+			<div className='flex items-center w-full justify-center'>
+				{!success && !error && <BeatLoader />}
+				<FormSuccess message={success} />
+				{!success && <FormError message={error} />}
+			</div>
+		</CardWrapper>
+	);
+};
